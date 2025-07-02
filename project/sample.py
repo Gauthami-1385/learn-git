37d7447f6ad4522a1c7be902bfcd0f39a59be427
Gauthami S <gauthami.s@gadgeon.com>
2025-07-02 21:22:53 +0530

    initial commit

---
+++ project/sample.py
@@ -0,0 +1,17 @@
+# Print all prime number in intervel
+x=int(input('Enter the lower limit'))
+y=int(input('Enter the upper limit'))
+
+prime=[True] * (y+1)
+
+prime[0],prime[1]=False,False
+
+for i in range(2,int(y**0.5)+1):
+    if prime[i]:
+        for j in range(i*i,y+1,i):
+            prime[j]=False
+
+res=[i for i in range(x,y+1)if prime[i]]
+
+print(res if res else 'No')
+        
